386b0e980e36b10f440d05ba0e8a998f82ea1d94
jewel12 <xiedajeeling@gmail.com>
2012-01-11 01:44:32 +0900

    リファクタリング

--- src/script.js
+++ src/script.js
@@ -1,34 +1,33 @@
-var dds = document.getElementsByTagName('dd');
-
-var addFaceInfo = function() {
-    var regexp = /^声[ \-：:]*(.+)[、,]*.*$/;
-    return function( dd ) {
-	var xhr = new XMLHttpRequest();
-	if ( regexp.test( dd.innerText ) ) {
-	    console.log( RegExp.$1 );
-	    getImageUrl( RegExp.$1, appendFace );
-	}
-
-	function appendFace() {
-	    if( xhr.readyState == 4 ) {
-		var resp = JSON.parse(xhr.responseText);
-		var image_url = resp.responseData.results[0].tbUrl;
-		var image = document.createElement( 'img' );
-		image.src = image_url;
-		dd.appendChild( image );
+(function () {
+    var addFaceInfo = function () {
+	var regexp = /^声[ \-：:]*(.+)[、,\(\)]*.*$/;
+	return function ( dd ) {
+	    function appendFace() {
+		if( xhr.readyState == 4 ) {
+		    var resp = JSON.parse(xhr.responseText),
+		    image_url = resp.responseData.results[0].tbUrl,
+		    image = document.createElement( 'img' );
+		    image.src = image_url;
+		    dd.appendChild( image );
+		}
 	    }
-	}
 
-	function getImageUrl( query, callback ) {
-	    xhr.open( 'GET', 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=1&imgtype=face&q=' + query, true );
-	    xhr.onreadystatechange = callback;
-	    xhr.send();
-	}
+	    function getImageUrl( query, callback ) {
+		xhr.open( 'GET', 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=1&imgtype=face&q=声優 ' + query, true );
+		xhr.onreadystatechange = callback;
+		xhr.send();
+	    }
 
+	    var xhr = new XMLHttpRequest();
+	    if ( regexp.test( dd.innerText ) ) {
+		console.log( RegExp.$1 );
+		getImageUrl( RegExp.$1, appendFace );
+	    }
+	};
+    }();
+
+    var dds = document.getElementsByTagName('dd');
+    for ( var i = 0, dds_length = dds.length; i < dds_length; i++) {
+	addFaceInfo( dds[i] );
     }
-}();
-
-for( var i=0; i < dds.length; i++ ){
-    addFaceInfo( dds[i] );
-}
-
+})();
