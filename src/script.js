b38cd5cb9e1ea0d0d601f96b2c08d7c73919d605
jewel12 <xiedajeeling@gmail.com>
2012-01-12 21:37:01 +0900

    refactoring

--- src/script.js
+++ src/script.js
@@ -1,33 +1,34 @@
 (function () {
-    var addFaceInfo = function () {
-	var regexp = /^声[ \-：:]*(.+)[、,\(\)]*.*$/;
-	return function ( dd ) {
-	    function appendFace() {
-		if( xhr.readyState == 4 ) {
-		    var resp = JSON.parse(xhr.responseText),
-		    image_url = resp.responseData.results[0].tbUrl,
-		    image = document.createElement( 'img' );
-		    image.src = image_url;
-		    dd.appendChild( image );
-		}
-	    }
+     var addFaceInfo = function () {
+	 var regexp = /^声[ \-：:]*(.+)[、,\(\)]*.*$/;
+	 return function ( dd ) {
+	     function appendFace() {
+		 if( xhr.readyState == 4 ) {
+		     var resp = JSON.parse(xhr.responseText),
+		     image_url = resp.responseData.results[0].tbUrl,
+		     image = document.createElement( 'img' );
+		     image.src = image_url;
+		     dd.appendChild( image );
+		 }
+	     }
 
-	    function getImageUrl( query, callback ) {
-		xhr.open( 'GET', 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=1&imgtype=face&q=声優 ' + query, true );
-		xhr.onreadystatechange = callback;
-		xhr.send();
-	    }
+	     function getImageUrl( query, callback ) {
+		 xhr.open( 'GET', 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=1&imgtype=face&q=声優 ' + query, true );
+		 xhr.onreadystatechange = callback;
+		 xhr.send();
+	     }
 
-	    var xhr = new XMLHttpRequest();
-	    if ( regexp.test( dd.innerText ) ) {
-		console.log( RegExp.$1 );
-		getImageUrl( RegExp.$1, appendFace );
-	    }
-	};
-    }();
+	     var xhr = new XMLHttpRequest();
+	     if ( regexp.test( dd.innerText ) ) {
+		 console.log( RegExp.$1 );
+		 getImageUrl( RegExp.$1, appendFace );
+	     }
+	 };
+     }();
 
-    var dds = document.getElementsByTagName('dd');
-    for ( var i = 0, dds_length = dds.length; i < dds_length; i++) {
-	addFaceInfo( dds[i] );
-    }
-})();
+     var dds = document.getElementsByTagName('dd');
+     var i = 0, dds_length = dds.length;
+     for (; i < dds_length; i++) {
+	 addFaceInfo( dds[i] );
+     }
+ })();
